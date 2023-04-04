274d22481f3dbe3af3a0fe777832e61830445732
kawhi <CodingWell@gmail.com>
2023-04-04 14:51:27 +0800

    update: clean code

--- src/output.js
+++ src/output.js
@@ -6,24 +6,6 @@ function lessToJsTemplate(opts, data) {
     } = opts
 
     switch (mode) {
-        case 'node':
-            return `module.exports = ${data}`
-        case 'json':
-            return `${data}`
-        case 'es6':
-            return `export default ${data}`
-
-        default:
-            return `export default ${data}`
-    }
-}
-
-function lessToJsTemplate(opts, data) {
-    const {
-        mode
-    } = opts
-
-    switch (mode) {
         case 'commonjs':
             return `module.exports = ${data}`
         case 'json':
